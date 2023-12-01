b1fd5fa3bdaf9ed98504f1d6c9b5fa542f86fee5
M. Daffa Teuku F.A <111522987+pixelwhiz@users.noreply.github.com>
2023-12-01 21:24:09 +0700

    .

--- frontend/src/components/client/Navbar.vue
+++ frontend/src/components/client/Navbar.vue
@@ -9,7 +9,7 @@
         <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
           <li><a>Home</a></li>
           <li>
-            <a>Parent</a>
+            <a>Parents</a>
             <ul class="p-2">
               <li><a>Submenu 1</a></li>
               <li><a>Submenu 2</a></li>
