eab2309af088401d99f94699a3caf139a3cbf6e8
M. Daffa Teuku F.A <111522987+pixelwhiz@users.noreply.github.com>
2023-12-02 11:48:09 +0700

    .

--- backend/models/Index.js
+++ backend/models/Index.js
@@ -5,7 +5,6 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.DIALECT,
     port: dbConfig.PORT,
-
     pool: {
         max: dbConfig.pool.max,
         min: dbConfig.pool.min,
